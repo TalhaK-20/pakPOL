4a776058109eeb3b05747b9f12888de0d5604310
Talha Khalid <F2021266625@umt.edu.pk>
2024-11-05 23:36:32 +0500

    Minor Changes

--- index.js
+++ index.js
@@ -1884,7 +1884,7 @@ app.get("/criminals", async (req, res) => {
       if (words.length > 0) {
         filterCriteria.Criminal_Crime = {
           $regex: words.join('|'),  // Use regex OR to match any word
-          $options: 'i'  // Case insensitive
+          $options: 'i'  // Case Insensitive
         };
 
       }
